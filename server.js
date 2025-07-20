2199ee1b2d406e8dd7b1c7ba2eb21120a5a508d0
IsabelleRouxelin <isabelle.rouxelin@gmail.com>
2025-07-20 17:17:02 +0100

    working through server file

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
 //Main express backend
+//import required modules
+//create an instance of an express application 
+//define the port
+// 
+//implement CRUD Operations for notes
+
+// create
+
+//read
+
+// update
+
+//delete
