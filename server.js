f84aafd859943b320df3ffca58da6b6f474ab1ed
IsabelleRouxelin <isabelle.rouxelin@gmail.com>
2025-07-21 19:23:43 +0100

    removed wildcard

--- server.js
+++ server.js
@@ -59,11 +59,6 @@ app.post("/echo", (req, res) => {
   res.json({ received: req.body });
 });
 
-// Wildcard route to handle undefined routes
-app.all("*", (req, res) => {
-  res.status(404).send("Route not found");
-});
-
 // Start the server and listen on the specified port
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
