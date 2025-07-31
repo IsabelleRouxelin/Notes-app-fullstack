cf7d3f9bb23f587b7eb59848f7fa48fdabe6f4f5
IsabelleRouxelin <isabelle.rouxelin@gmail.com>
2025-08-01 00:51:36 +0100

    created server.js file

---
+++ public/server.js
@@ -0,0 +1,13 @@
+// import modules
+
+// create an instance of express
+// create port
+//add middleware to parse json requests
+//read notes
+// write notes
+//service static files from public directory
+// get notes
+//post notes - create note
+//error handling
+// put note - update note
+// delete a note
